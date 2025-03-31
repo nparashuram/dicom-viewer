12abd4da593cee48bcfff586165ac830c640b3e3
Parashuram <code@nparashuram.com>
2025-03-30 22:53:56 -0400

    [android] Converted viewer to grid

--- android/app/src/main/java/com/nparashuram/dicomviewer/ui/ViewerScreen.kt
+++ android/app/src/main/java/com/nparashuram/dicomviewer/ui/ViewerScreen.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
@@ -50,27 +52,39 @@ fun ViewerScreen(location: String, viewModel: PDicomViewModel, onClose: () -> Un
             Button(onClick = { onClose() }) { Text("Cancel") }
         }
     } else {
-        LazyColumn {
-            item {
-                Row(
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text("Viewer Screen", fontSize = 20.sp, modifier = Modifier.padding(5.dp))
-                    OutlinedButton(onClick = { onClose() }) { Text(text = "x") }
+        LazyVerticalGrid(columns = GridCells.Adaptive(minSize = 300.dp)) {
+            item(span = { GridItemSpan(this.maxLineSpan) }) {
+                Column {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text("Viewer Screen", fontSize = 20.sp, modifier = Modifier.padding(5.dp))
+                        OutlinedButton(onClick = { onClose() }) { Text(text = "x") }
+                    }
+                    Text("Location: $location")
                 }
-                Text("Location: $location")
             }
-
-            items(items = Plane.entries) { plane ->
+            items(Plane.entries) { plane ->
                 selectedSliceIndex[plane]?.let {
                     Row(modifier = Modifier.fillMaxWidth()) {
                         ImageSlice(plane, selectedSliceImg[plane])
-                        ImageSliceSelector(
-                            plane,
-                            selectedSliceIndex[plane] ?: 0,
-                            selectedPDicom.files.getSlice(plane).size
-                        ) { plane, value, context ->
-                            viewModel.updateSelectedSlice(plane, value, context)
+                    }
+                }
+            }
+            item {
+                Column {
+                    Plane.entries.map { plane ->
+                        selectedSliceIndex[plane]?.let {
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Text(text = plane.name)
+                                ImageSliceSelector(
+                                    plane,
+                                    selectedSliceIndex[plane] ?: 0,
+                                    selectedPDicom.files.getSlice(plane).size
+                                ) { plane, value, context ->
+                                    viewModel.updateSelectedSlice(plane, value, context)
+                                }
+                            }
                         }
                     }
                 }
